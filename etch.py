185e1de5f342163677666d942476b20199899ac8
patzaa <patzer__@hotmail.de>
2018-09-05 21:12:21 -0400

    Homework 1 added

---
+++ etch.py
@@ -0,0 +1,35 @@
+#!usr/bin/env python3
+
+import pygame, sys
+from pygame.locals import *
+
+x=150
+y=100
+
+pygame.init()
+screen = pygame.display.set_mode((300,200))
+
+
+clock = pygame.time.Clock()
+screen.fill((221,221,221))
+
+while 1:
+    clock.tick(60)
+    pygame.draw.triangle(screen,(0,0,0), (x,y), 2)
+    pygame.display.update()
+   
+   
+    key=pygame.key.get_pressed()
+    if key[pygame.K_RIGHT]:x+=1
+    if key[pygame.K_LEFT]:x-=1
+    if key[pygame.K_UP]:y-=1
+    if key[pygame.K_DOWN]:y+=1 
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+           sys.exit()
+        elif event.type == KEYDOWN and event.key == K_ESCAPE:
+            sys.exit()
+        elif event.type == KEYDOWN and event.key == K_SPACE:
+            screen.fill((255,255,255))
+
